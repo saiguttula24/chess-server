ca33f9633669a71cc764a21b3e504ac63cb5d426
sai guttula <saiguttula@sais-MacBook-Air.local>
2024-03-09 20:20:00 +0530

    commit-2

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,31 +1,34 @@
 import { Request, Response } from 'express';
 const bcrypt = require('bcrypt');
 const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
 const { signupSchema,loginSchema } = require('../utils/validations');
 
 class AuthController {
 
-  async login(req: Request, res: Response) {
+  async login(req:Request, res:Response) {
     try {
-        const { error, value } = loginSchema.validate(req.body);
-        if (error) {
-          return res.status(400).json({ message: error.details[0].message });
-        }
-    
-        const user = await User.findOne({ email: value.email });
-        if (!user) {
-          return res.status(404).json({ message: 'User not found' });
-        }
-    
-        const passwordMatch = await bcrypt.compare(value.password, user.password);
-        if (!passwordMatch) {
-          return res.status(401).json({ message: 'Incorrect password' });
-        }
-    
-        res.status(200).json({ message: 'Login successful' });
+      const { error, value } = loginSchema.validate(req.body);
+      if (error) {
+        return res.status(400).send({ message: error.details[0].message });
+      }
+  
+      const user = await User.findOne({ email: value.email });
+      if (!user) {
+        return res.status(404).send({ message: 'User not found' });
+      }
+  
+      const passwordMatch = await bcrypt.compare(value.password, user.password);
+      if (!passwordMatch) {
+        return res.status(401).send({ message: 'Incorrect password' });
+      }
+  
+      const token = jwt.sign({ userId: user._id }, 'your_secret_key', { expiresIn: '1h' });
+  
+      res.status(200).send({ message: 'Login successful', token: token });
     } catch (error) {
-        console.error('Error during login:', error);
-        res.status(500).json({ message: 'Internal server error' });
+      console.error('Error during login:', error);
+      res.status(500).send({ message: 'Internal server error' });
     }
   }
 
@@ -33,12 +36,12 @@ class AuthController {
     try {
         const { error, value } = signupSchema.validate(req.body);
         if (error) {
-          return res.status(400).json({ message: error.details[0].message });
+          return res.status(400).send({ message: error.details[0].message });
         }
     
         const existingUser = await User.findOne({ email: value.email });
         if (existingUser) {
-          return res.status(400).json({ message: 'Email already registered' });
+          return res.status(400).send({ message: 'Email already registered' });
         }
     
         const hashedPassword = await bcrypt.hash(value.password, 10);
@@ -46,10 +49,10 @@ class AuthController {
         const newUser = new User({ username: value.username, email: value.email, password: hashedPassword });
         await newUser.save();
     
-        res.status(201).json({ message: 'User created successfully' });
+        res.status(201).send({ message: 'User created successfully' });
     } catch (error) {
         console.error('Error during signup:', error);
-        res.status(500).json({ message: 'Internal server error' });
+        res.status(500).send({ message: 'Internal server error' });
     }
   }
 
